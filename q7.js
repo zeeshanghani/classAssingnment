86ac88606b0edbd43ff58318c19a04c7f8bf08dd
zeeshanghani <mzeeshanghani786@gmail.com>
2023-09-01 23:28:39 -0700

    Message

--- q7.js
+++ q7.js
@@ -2,3 +2,16 @@
 //Q7 - Create a function that takes an array of numbers as parameter. 
 //Use a while loop to calculate and return 
 //the sum of all the numbers in the array
+var numbers = [2, 2, 2, 2, 2];
+function sumofAll(numbers) {
+    let totalSum = 0;
+    let index = 0;
+    while (index < numbers.length) {
+        totalSum = totalSum + numbers[index];
+        index++;
+    }
+    return totalSum;
+    //console.log(totalSum)
+}
+var grandtotal = sumofAll(numbers);
+console.log(grandtotal);
--- q7.ts
+++ q7.ts
@@ -1,3 +1,20 @@
 //Q7 - Create a function that takes an array of numbers as parameter. 
 //Use a while loop to calculate and return 
 //the sum of all the numbers in the array
+
+var numbers:number[]=[2,2,2,2,2]
+function sumofAll(numbers:number[])
+{
+    let totalSum:number=0
+    let index:number=0
+    while (index < numbers.length)
+    {
+        totalSum =totalSum+numbers[index];
+        index++;
+    }
+    return totalSum
+    //console.log(totalSum)
+    
+}
+var grandtotal:number=sumofAll(numbers)
+console.log(grandtotal)
